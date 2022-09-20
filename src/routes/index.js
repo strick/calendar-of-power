645a1add5d8e877b3aece94f0478064cb06de48f
Brian Strickland <bstrick@gmail.com>
2022-09-20 16:45:19 -0400

    Added slim form

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Calendar of Power' });
+  res.render('index', { 
+    title: 'Calendar of Power',
+    formElements: {
+        eventTitle: "Event Title",
+        eventStartDate: "Start Date",
+        eventEndDate: "End Date",
+        eventStartTime: "Start Time",
+        eventEndTime: "End Time"
+    }
+    });
 });
 
 module.exports = router;
